7d2c69cd4ce5f2de1e087487ad33523f99a30c79
KimMarinet <kiana507680@gmail.com>
2025-04-04 15:47:28 +0900

    HTML, CSS End

---
+++ 02 프론트앤드 프로그래밍/shopping/js/common.js
@@ -0,0 +1,18 @@
+window.addEventListener("DOMContentLoaded", function(){
+    new Swiper(".main-banner", {
+        loop: true,
+        navigation: {
+          nextEl: ".swiper-button-next",
+          prevEl: ".swiper-button-prev",
+        },
+        pagination: {
+            el: ".swiper-pagination",
+            clickable : true,
+          },
+        autoplay: {
+            delay: 2500,
+            disableOnInteraction: false,
+          },
+      });
+      
+});
