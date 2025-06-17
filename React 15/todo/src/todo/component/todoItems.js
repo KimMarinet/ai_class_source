8861f91eb663c4dfa7aefc8cbf4c4202e6e56d31
KimMarinet <kiana507680@gmail.com>
2025-06-17 18:16:56 +0900

    20250617 E

--- React 15/todo/src/todo/component/todoItems.js
+++ React 15/todo/src/todo/component/todoItems.js
@@ -1,13 +1,51 @@
-const todoItems = ({items}) => {
+import TodoItem from './todoItem';
+import { FaInfoCircle } from "react-icons/fa";
+
+const TodoItems = ({ items, onToggle, onRemove, onRemoveAll }) => {
+  if(!items || items.length === 0){
+    return(
+      <div>
+        <FaInfoCircle /> 등록된 스케쥴이 없습니다
+      </div>
+    );
+  }
+
   return (
-    <ul>
-      {items.map((item) => (
-        <li key={item.id}>
-          {item.title} / {item.content}
-        </li>
-      ))}
-    </ul>
+    <>
+      <ul>
+        {items.map((props) => (
+          <TodoItem
+            key={props.id}
+            {...props}
+            onToggle={onToggle}
+            onRemove={onRemove}
+          />
+        ))}
+      </ul>
+      <button type='button' onClick={onRemoveAll}>
+        선택 스케쥴 삭제
+      </button>
+    </>
   );
 };
 
-export default todoItems;
+// const TodoItems = ({ items }) => {
+//   return (
+//     <ul>
+//       {items.map((props) => (
+//         <>
+//           <TodoItem {...props} />
+//           {/*<TodoItem id={id} title={title} content={content} checked={checked} /> */}
+//           {/*<li
+//           key={id} // 한줄 주석
+//         >
+//           {checked ? <MdCheckBox /> : <MdCheckBoxOutlineBlank />}
+//           {title} / {content}
+//         </li> */}
+//         </>
+//       ))}
+//     </ul>
+//   );
+// };
+
+export default TodoItems;
--- React 15/todo/src/todo/containers/TodoContainer.js
+++ React 15/todo/src/todo/containers/TodoContainer.js
@@ -1,29 +1,84 @@
 import { useState } from 'react';
 import TodoForm from '../component/todoForm';
 import TodoItems from '../component/todoItems';
+import { HiOutlineTemplate } from 'react-icons/hi';
 
 const TodoContainer = () => {
+  const [form, setForm] = useState({});
+  const [items, setItems] = useState([
+    { id: 1, title: '할일1', content: '할일1 내용', checked: false },
+    { id: 2, title: '할일2', content: '할일2 내용', checked: true },
+    { id: 3, title: '할일3', content: '할일3 내용', checked: false },
+  ]);
+  const [errors, setErrors] = useState({});
 
-    const [form, setForm] = useState({});
-    const [items, setItems] = useState([]);
+  const onSubmit = (e) => {
+    e.preventDefault();
 
-    const onSubmit = (e) => {
-      e.preventDefault();
-      
-      setItems(items.concat({ ...form, id: Date.now() }));
+    // 유효성 검사
+    let hasErrors = false;
+    const requiredFields = {
+      title: '제목을 입력하세요.',
+      content: '내용을 입력하세요.',
     };
+    const errors = {};
+    for (const [field, message] of Object.entries(requiredFields)) {
+      if (!form[field] || !form[field]?.trim()) {
+        errors[field] = message;
+        hasErrors = true;
+      }
+    }
 
-    const onChange = (e) => {
-      setForm({ ...form, [e.target.name]: e.target.value });
-    };
+    // 유효성 검사 실패시 다음 로직 실행 X
+    setErrors(errors);
 
-    return (
-      <>
-        <TodoForm onSubmit={onSubmit} onChange={onChange} form={form} />
-        <TodoItems items={items} />
-        todo: {form.title} / todoContent: {form.content}
-      </>
+    if (hasErrors) return;
+
+    setItems(items.concat({ ...form, id: Date.now() }));
+
+    // 양식 초기화
+    setForm({});
+  };
+
+  const onChange = (e) => {
+    setForm({ ...form, [e.target.name]: e.target.value });
+  };
+
+  // 체크박스 토클 처리
+  const onToggle = (id) => {
+    setItems((prevItems) =>
+      prevItems.map((item) =>
+        item.id === id ? { ...item, checked: !item.checked } : item,
+      ),
     );
+  };
+
+  // 스케줄 하나 삭제 처리
+  const onRemove = (id) => {
+    setItems((prevItems) => prevItems.filter((item) => item.id !== id));
+  };
+
+  // 선택 스케쥴 일괄 삭제 처리
+  const onRemoveAll = () => {
+    setItems(items.filter(({checked}) => !checked));
+  }
+
+  return (
+    <>
+      <TodoForm
+        onSubmit={onSubmit}
+        onChange={onChange}
+        form={form}
+        errors={errors}
+      />
+      <TodoItems 
+        items={items} 
+        onToggle={onToggle} 
+        onRemove={onRemove}
+        onRemoveAll = {onRemoveAll} 
+      />
+    </>
+  );
 };
 
 export default TodoContainer;
