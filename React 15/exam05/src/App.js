cc9709b7d50adfb844e94428b7b469832cf2dab3
KimMarinet <kiana507680@gmail.com>
2025-06-27 17:18:56 +0900

    20250627 react E

--- React 15/exam05/src/App.js
+++ React 15/exam05/src/App.js
@@ -1,12 +1,14 @@
 import MyComponent1 from "./classNames/MyComponent1";
 import CSSModule from "./CSSModule/CSSModule";
 import SassComponent from "./sass/SassComponent"
+import StyledComponent1 from "./styled/StyledComponent1";
 
 
 const App = () => {
   //return <SassComponent />
   //return <CSSModule />
-  return <MyComponent1 />
+  //return <MyComponent1 />
+  return <StyledComponent1 />
 };
 
 export default App;
